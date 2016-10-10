617d7c94f6879190f8f2ce10bd2b590913334a9b
Dflk <baran.romain@gmail.com>
2016-10-10 22:01:25 +0200

    d05

--- d05/ex14/ex14.sql
+++ d05/ex14/ex14.sql
@@ -1,1 +1,1 @@
-SELECT etage_salle as "etage", SUM(nbr_siege) as "siege" FROM salle GROUP BY etage_salle ORDER BY nbr_siege DESC;
+SELECT etage_salle as "etage", SUM(nbr_siege) as "sieges" FROM salle GROUP BY etage_salle ORDER BY nbr_siege DESC;
